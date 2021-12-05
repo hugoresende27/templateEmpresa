555d4216d7bda0b3c2fe797cfad1aa36e38da68a
hugoresende27 <Hugoresende27@gmail.com>
2021-12-05 14:54:04 +0000

    Update enviarEmail.php

--- enviarEmail.php
+++ enviarEmail.php
@@ -18,7 +18,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'profweb27@gmail.com';                     //SMTP username
-    $mail->Password   = 'Prodep97';                               //SMTP password
+    $mail->Password   = 'passTestes';                               //SMTP password
     //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail ->  SMTPSecure ='tls';
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
